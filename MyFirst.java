43d73bf1283e2d145901a14d960a3ef8703bdaec
Ajeet31git <yajeet60@gmail.com>
2023-01-18 17:01:19 +0530

    First commit

---
+++ MyFirst.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+public class MyFirst
+{
+    public static void  main(String args[])
+    {
+        System.out.println("hello World");
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+        System.out.println(args[3]);
+    }
+}
